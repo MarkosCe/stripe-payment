9cc961a7079cd198ec770ee37f97072c45e4af9a
MarkosCe <cesar.markos99@gmail.com>
2023-01-24 01:01:11 -0600

    add button payment

--- app/src/main/java/com/example/stripepayment/MainActivity.java
+++ app/src/main/java/com/example/stripepayment/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.stripepayment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton button = findViewById(R.id.button_payment);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Reaizar pago", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
     }
 }
